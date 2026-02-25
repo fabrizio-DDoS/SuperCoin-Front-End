f0243e110e23c530daa209f3faae2e209d915408
Fabrízio Machado de Moura <100778214+fabrizio-DDoS@users.noreply.github.com>
2026-02-25 10:00:44 -0300

    Add files via upload

---
+++ kali.js
@@ -0,0 +1,17 @@
+function openreceber(){
+    window.location.href = 'receber.html'
+}
+function openenviar(){
+    window.location.href = 'enviar.html'
+}
+function openperfil(){
+    window.location.href = 'perfil.html'
+}
+function openmain(){
+    window.location.href = 'index.html'
+}
+function openhist(){
+    window.location.href = 'historico.html'
+    alert('Sem transações por enquanto')
+}
+
